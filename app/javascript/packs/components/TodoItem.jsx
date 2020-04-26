0f443ffd382b796489a1cc20856d7f0239ce7ff0
Aleksandr Vinogradov <QQpy3ko@gmail.com>
2020-04-26 13:44:43 +0300

    add 1sec onChange bouncing join while updating todo item

--- app/javascript/packs/components/TodoItem.jsx
+++ app/javascript/packs/components/TodoItem.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import _ from "lodash";
 import axios from "axios";
 import setAxiosHeaders from "./AxiosHeaders";
 
@@ -19,10 +20,13 @@ class TodoItem extends React.Component {
   }
 
   handleChange() {
+    this.setState({
+      complete: this.completedRef.current.checked
+    });
     this.updateTodoItem();
   }
-  updateTodoItem() {
-    this.setState({ complete: this.completedRef.current.checked });
+
+  updateTodoItem = _.debounce(() => {
     setAxiosHeaders();
     axios
     .put(this.path, {
@@ -35,7 +39,7 @@ class TodoItem extends React.Component {
     .catch(error => {
       console.log(error);
     });
-  }
+  }, 1000);
 
   handleDestroy() {
     setAxiosHeaders();
